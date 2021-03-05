8375e6b3a5cfde3c250d63a08372e8472c6fdbb7
Giuditta <giudittamarino89@gmail.com>
2021-03-05 15:20:37 +0100

    Esercizio base terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,16 @@ var fizzbuzz = "FizzBuzz";
 // SCORRO TUTTI I  NUMERI DA 1 A 100 E CONTROLLO SE I NUMERI SONO MULTIPLI DI 3 E 5(CONDIZIONE PIU' STRINGENTE), ALLORA STAMPO "FIZZBUZZ", SE SONO MULTIPLI DI 3 ALLORA STAMPO "FIZZ", SE SONO MULTIPLI DI 5, ALLORA STAMPO "BUZZ", ALTRIMENTI STAMPO I NUMERI PER COME SONO (i)
 for (var i = 1; i <= 100; i++) {
 if (!(i % 3) && !(i % 5)) {
+    document.getElementById('lista-numeri').innerHTML += "<li>" + fizzbuzz + "</li>";
     console.log(fizzbuzz);
   } else if (!(i % 3)) {
-      console.log(fizz);
+    document.getElementById('lista-numeri').innerHTML += "<li>" + fizz + "</li>";
+    console.log(fizz);
   } else if (!(i % 5)) {
+    document.getElementById('lista-numeri').innerHTML += "<li>" + buzz + "</li>";
     console.log(buzz);
   } else {
+    document.getElementById('lista-numeri').innerHTML += "<li>" + i + "</li>";
     console.log(i);
   }
 }
