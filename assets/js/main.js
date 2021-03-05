b58297f08fc891169d10d661f756931cba60f371
Giuditta <giudittamarino89@gmail.com>
2021-03-05 14:57:36 +0100

    esercizio base terminato

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// CREO UNA STRINGA "FIZZ" E LA SALVO IN UNA VARIABILE
+var fizz = "Fizz";
+
+// CREO UNA STRINGA "BUZZ" E LA SALVO IN UNA VARIABILE
+var buzz = "Buzz";
+
+// CREO UNA STRINGA "FIZZBUZZ" E LA SALVO IN UNA VARIABILE
+var fizzbuzz = "FizzBuzz";
+
+// SCORRO TUTTI I  NUMERI DA 1 A 100 E CONTROLLO SE I NUMERI SONO MULTIPLI DI 3 E 5(CONDIZIONE PIU' STRINGENTE), ALLORA STAMPO "FIZZBUZZ", SE SONO MULTIPLI DI 3 ALLORA STAMPO "FIZZ", SE SONO MULTIPLI DI 5, ALLORA STAMPO "BUZZ", ALTRIMENTI STAMPO I NUMERI PER COME SONO (i)
+for (var i = 1; i <= 100; i++) {
+if (!(i % 3) && !(i % 5)) {
+    console.log(fizzbuzz);
+  } else if (!(i % 3)) {
+      console.log(fizz);
+  } else if (!(i % 5)) {
+    console.log(buzz);
+  } else {
+    console.log(i);
+  }
+}
